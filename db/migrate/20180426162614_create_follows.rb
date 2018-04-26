85f52293df4f7b6211da6bd5d1c1384bee6701d2
atwill <ykinoshi1265@gmail.com>
2018-04-27 01:52:10 +0900

    model作成

---
+++ db/migrate/20180426162614_create_follows.rb
@@ -0,0 +1,9 @@
+class CreateFollows < ActiveRecord::Migration[5.0]
+  def change
+    create_table :follows do |t|
+      t.references :user,     null: false, foreign_key: true
+      t.integer :follow_id,  null: false
+      t.timestamps
+    end
+  end
+end
---
+++ test/models/contribute_like_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ContributeLikeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
