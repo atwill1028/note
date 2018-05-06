708ce1ae86a7576e9885a5d026dbf0f15c774ea9
atwill <ykinoshi1265@gmail.com>
2018-05-06 15:35:08 +0900

    hoverを全てCSSに記述 routeを全てresources

--- app/assets/javascripts/contribute.js
+++ app/assets/javascripts/contribute.js
@@ -1,22 +1,4 @@
 $(function(){
-  $(".navigation_menu.not_selected, .navigation_question").hover(
-    function(){
-      $(this).css("color", "#222222");
-      $(this).css("border-bottom", "1px solid black");
-    },
-    function(){
-      $(this).css("color", "#787c7b");
-      $(this).css("border", "none");
-    }
-  )
-  $(".category_menu").hover(
-    function(){
-      $(this).addClass("active");
-    },
-    function(){
-      $(this).removeClass("active");
-    }
-  )
   $(".search__button").click(function(){
     $(this).replaceWith('<input class="search__window"></input>');
     $(".search__window").css("border-radius", "100px");
--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Rails.application.routes.draw do
   root "contributes#index"
   resources :contributes, only: [:index, :new, :create] do
     collection do
-      get :manga
-      get :column
-      get :novel
-      get :photo
-      get :music
-      get :business
-      get :lifestyle
-      get :tech
-      get :entertainment
-      get :pickup
-      get :categories
-      get :hashtag
-      get :help
+      resources :manga, only: [:index]
+      resources :column, only: [:index]
+      resources :novel, only: [:index]
+      resources :photo, only: [:index]
+      resources :music, only: [:index]
+      resources :business, only: [:index]
+      resources :lifestyle, only: [:index]
+      resources :tech, only: [:index]
+      resources :entertainment, only: [:index]
+      resources :pickup, only: [:index]
+      resources :categories, only: [:index]
+      resources :hashtag, only: [:index]
+      resources :help, only: [:index]
     end
   end
 end
