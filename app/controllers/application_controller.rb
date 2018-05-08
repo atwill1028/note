683ab43fcf5ca75ad6d01a934ef6fd75eafb9bed
atwill <ykinoshi1265@gmail.com>
2018-05-08 21:39:29 +0900

    @変数をcontrollerから消してviewで持つ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :get_category
-  before_action :get_current_user_id, if: :user_signed_in?
-  before_action :get_current_user_image, if: :user_signed_in?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
@@ -13,14 +11,6 @@ class ApplicationController < ActionController::Base
 
   def get_category
     @category = params[:controller]
-  end
-
-  def get_current_user_id
-    @current_user_id = current_user.id
-  end
-
-  def get_current_user_image
-    @current_user_image = current_user.image
   end
 
   def configure_permitted_parameters
