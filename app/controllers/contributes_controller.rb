65c9ca8dc5094067ce5735cc0737a493b8d9ee7b
atwill <ykinoshi1265@gmail.com>
2018-05-13 13:54:56 +0900

    contributes_new画面のview作成 ##WHAT 投稿新規作成画面の作成 ##WHY 投稿の新規作成を可能にするため

--- app/controllers/contributes_controller.rb
+++ app/controllers/contributes_controller.rb
@@ -1,8 +1,20 @@
 class ContributesController < ApplicationController
-  before_action :get_category
+  before_action :set_user
+  def new
+    @contribute = Contribute.new
+  end
+
+  def create
+    @contribute = @user.contributes.new(contribute_params)
+    @contribute.save
+  end
 
   private
-  def get_category
-    @category= params[:action]
+  def contribute_params
+    params.require(:contribute).permit(:image, :title, :content).merge(user_id: current_user.id)
+  end
+
+  def set_user
+    @user = User.find(current_user.id)
   end
 end
