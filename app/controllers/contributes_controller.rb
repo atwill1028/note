09b7a8d5966f7cf7632272ced00816a194f27463
atwill <ykinoshi1265@gmail.com>
2018-05-15 21:05:18 +0900

    @user→current_user

--- app/controllers/contributes_controller.rb
+++ app/controllers/contributes_controller.rb
@@ -1,12 +1,11 @@
 class ContributesController < ApplicationController
-  before_action :set_user
 
   def new
     @contribute = Contribute.new
   end
 
   def create
-    @contribute = @user.contributes.build(contribute_params)
+    @contribute = current_user.contributes.build(contribute_params)
     if @contribute.save
       redirect_to new_contribute_path
     else
@@ -17,9 +16,5 @@ class ContributesController < ApplicationController
   private
   def contribute_params
     params.require(:contribute).permit(:image, :title, :content)
-  end
-
-  def set_user
-    @user = User.find(current_user.id)
   end
 end
