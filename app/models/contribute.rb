243f65d39095039ae3ed8530c765bf6eb9debdf0
atwill <ykinoshi1265@gmail.com>
2018-06-13 20:34:53 +0900

    <レビューを受けて修正＞ 画像をローカルに保存して、それを指定 画像のwidthをcssで指定するように変更

--- app/models/contribute.rb
+++ app/models/contribute.rb
@@ -2,7 +2,7 @@ class Contribute < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :contribute_likes, dependent: :destroy
-
+  mount_uploader :image, ImageUploader
   def like_user(user_id)
     contribute_likes.find_by(user_id: user_id)
   end
