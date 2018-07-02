e7c3ac21434296c0620e018657645a57f51a9bcd
atwill <ykinoshi1265@gmail.com>
2018-07-03 00:57:23 +0900

    <route> sider対策

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
       resources :helps, only: :index
     end
   end
-  resources :hashtags, param: :hashtag, path: '/', only: [:index, :show]
+  resources :hashtags, param: :hashtag, path: '/', only: :show
   resources :users, only: [:index, :show]
 end
