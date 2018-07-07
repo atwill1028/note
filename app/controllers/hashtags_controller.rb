ee2c1abba0bacef437261e2c07467c000dada0c1
atwill <ykinoshi1265@gmail.com>
2018-07-07 21:43:36 +0900

    <hashtags_controller> ハッシュタグ人気順画面用のアクションを追加

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,19 +1,25 @@
 class HashtagsController < ApplicationController
-  before_action :get_category, :get_hashtag
+  before_action :get_hashtag, :show_and_popular
 
   def show
     @contained_hashtag_contributes = Contribute.joins(:contribute_tags).where(contribute_tags: {name: get_hashtag}).order("contributes.created_at DESC")
     @contained_hashtag_contributes_count = @contained_hashtag_contributes.count
-    @likes = ContributeLike.all.includes(:user)
-    @content_max_length = 200
+    @sort_type = "new"
+  end
+
+  def popular
+    @contained_hashtag_contributes = Contribute.joins(:contribute_tags).where(contribute_tags: {name: get_hashtag}).order("likes_count DESC, created_at DESC")
+    @contained_hashtag_contributes_count = @contained_hashtag_contributes.count
   end
 
   private
-  def get_category
-    @category = params[:controller]
-  end
 
   def get_hashtag
     @hashtag = params[:hashtag]
+  end
+
+  def show_and_popular
+    @likes = ContributeLike.all.includes(:user)
+    @content_max_length = 200
   end
 end
