b58f40f0622dc54984541456198ffc18cb5f8bc2
atwill <ykinoshi1265@gmail.com>
2018-07-08 12:36:45 +0900

    <レビューを受けて修正> インスタンス変数→グローバル変数

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,10 +1,9 @@
 class HashtagsController < ApplicationController
-  before_action :get_hashtag, :show_and_popular
+  before_action :get_hashtag
 
   def show
     @contained_hashtag_contributes = Contribute.joins(:contribute_tags).where(contribute_tags: {name: get_hashtag}).order("contributes.created_at DESC")
     @contained_hashtag_contributes_count = @contained_hashtag_contributes.count
-    @sort_type = "new"
   end
 
   def popular
@@ -18,8 +17,4 @@ class HashtagsController < ApplicationController
     @hashtag = params[:hashtag]
   end
 
-  def show_and_popular
-    @likes = ContributeLike.all.includes(:user)
-    @content_max_length = 200
-  end
 end
